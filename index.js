8fc8463a5d128c49fa41a0e9f8f55b3c3418abcd
Abdulmazid24 <a.m.abdulmazid24@gmail.com>
2025-03-22 01:20:31 +0600

    some admin related apis and jwt emplementd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +36,15 @@ async function run() {
     const paymentsCollection = db.collection('payments');
     const tasksCollection = db.collection('tasks');
     const messagesCollection = db.collection('messages');
+
+    // jwt related apis
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      });
+      res.send({ token });
+    });
     // users related apis
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -48,6 +58,73 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    // verify token middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+    // use verify admin after verify token
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    };
+    app.get('/user/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    });
+    // Admin only apis
+    app.get(
+      '/all-employee-list',
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        console.log(req.headers);
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      }
+    );
+    app.patch(
+      '/employee/hr/:id',
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'HR',
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+    );
     app.post('/work-sheet', async (req, res) => {
       const tasks = req.body;
       const result = await tasksCollection.insertOne(tasks);
